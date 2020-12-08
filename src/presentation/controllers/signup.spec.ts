bb6b5cd621365ca0a513a8f70f736c3bf1114acf
Lincoln Gadea <lincoln@glinc.com.br>
2020-12-08 00:17:35 -0300

    test: ensure SignUpController returns 500 if EmailValidator throw

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,3 +1,4 @@
+import { ServerError } from './../errors/server-error'
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-params-error'
 import { EmailValidator } from '../protocols/email-validator'
@@ -94,5 +95,27 @@ describe('Signup Controller', () => {
     }
     sut.handle(httpRequest)
     expect(isValidSpy).toHaveBeenCalledWith('any@email.com')
+  })
+
+  test('Should return 500 if EmailValidator throws', () => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid (email: string): boolean {
+        throw new Error()
+      }
+    }
+
+    const emailValidatorStub = new EmailValidatorStub()
+    const sut = new SignUpController(emailValidatorStub)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
